7c54ad8e6501b559beea11f6807cb799535275d3
krishna10-dev <kk1930778@gmail.com>
2025-12-03 19:12:19 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+import express, { json } from 'express'
+
+const app = express()
+app.get('/api/health', (req,res)=> {
+    res.status(200).json({message:"Success"})
+});
+app.listen(3000, ()=> console.log('server is running on'))
